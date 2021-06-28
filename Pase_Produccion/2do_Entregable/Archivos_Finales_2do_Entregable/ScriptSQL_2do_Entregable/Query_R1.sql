60c7c43dedeb994ff6a68053ae69f4ec9f9706ce
LBROWNBR <bartoloricsi@gmail.com>
2021-06-28 01:22:02 -0500

    Cambios Finales a produccion

--- Pase_Produccion/2do_Entregable/Archivos_Finales_2do_Entregable/ScriptSQL_2do_Entregable/Query_R1.sql
+++ Pase_Produccion/2do_Entregable/Archivos_Finales_2do_Entregable/ScriptSQL_2do_Entregable/Query_R1.sql
@@ -1,3 +1,5 @@
+use dbmatricula;
+
 -- DROP TABLE IF EXISTS `tbl_certificados`;
 CREATE TABLE `tbl_certificados` (
   `CertID` int(11) NOT NULL AUTO_INCREMENT,
@@ -39,3 +41,59 @@ CREATE TABLE `tbl_certificados` (
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- select * from tbl_certificados;
+
+
+
+
+-- DROP PROCEDURE IF EXISTS sp_ins_matricula_v3;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_ins_matricula_v3`(IN `p_cur_ID` INT(11), IN `p_Mat_Nombre` VARCHAR(50), IN `p_Mat_Paterno` VARCHAR(50), IN `p_Mat_Materno` VARCHAR(50), IN `p_Mat_TipoDocIdent` VARCHAR(40), IN `p_Mat_DNI` varchar(9), IN `p_Mat_Profesion` VARCHAR(50), IN `p_Mat_Genero` VARCHAR(1), IN `p_Mat_GradoAcade` VARCHAR(25), IN `p_Mat_Corroe` VARCHAR(100), IN `p_Mat_Celular` INT(9), IN `p_Mat_Ruc` VARCHAR(11), IN `p_Mat_Entidad` VARCHAR(100), IN `p_Mat_Departamento` VARCHAR(3), IN `p_Mat_Provincia` VARCHAR(3), IN `p_Mat_Distrito` VARCHAR(3), IN `p_Mat_NivGobierno` VARCHAR(25), IN `p_Mat_Area` VARCHAR(100), IN `p_Mat_AreaOpcional` VARCHAR(100), IN `p_Mat_Cargo` VARCHAR(100), IN `p_Mat_Clasificacion` VARCHAR(25), IN `p_Mat_ModContrac` VARCHAR(15), IN `p_Mat_ContracOtros` VARCHAR(20), IN `p_Mat_NroOficio` VARCHAR(100), IN `p_Mat_NomArchivo` VARCHAR(200), IN `p_Mat_RutaArchivo` VARCHAR(50), IN `p_maximoReg` INT(11), OUT `msg_salida` VARCHAR(500))
+BEGIN
+        DECLARE V_CONTADOR INT ;
+        DECLARE V_CONTADOR_CORREO INT ;
+        DECLARE V_COUNT_PROF_email INT;
+		
+		DECLARE V_NOMBRE_CURSO VARCHAR(200);
+		DECLARE V_ANIO_CURSO INT;
+		DECLARE V_TOT_CURSO_NOM_ANIO INT;
+		DECLARE V_TOT_INSCRITO_CURSO_NOM_ANIO INT;
+		DECLARE V_TOT_MAX_INSCRITO INT;
+
+        SELECT COUNT(*) INTO V_CONTADOR FROM tbl_matricula WHERE Mat_DNI = p_Mat_DNI AND cur_ID = p_cur_ID;
+        SELECT COUNT(*) INTO V_CONTADOR_CORREO FROM tbl_matricula WHERE Mat_Correo = p_Mat_Corroe AND Mat_DNI != p_Mat_DNI;
+        SELECT COUNT(*) INTO V_COUNT_PROF_email FROM `tbl_profesor` WHERE `email` = p_Mat_Corroe;
+
+		SELECT trim(cur_Nombre) INTO V_NOMBRE_CURSO FROM tbl_curso WHERE cur_ID = p_cur_ID;
+        SELECT YEAR(cur_FechaInicio) INTO V_ANIO_CURSO FROM tbl_curso WHERE cur_ID = p_cur_ID;
+		
+		SELECT count(*) INTO V_TOT_CURSO_NOM_ANIO FROM tbl_curso WHERE Estado = 1 AND YEAR(cur_FechaInicio) = V_ANIO_CURSO AND cur_Nombre = V_NOMBRE_CURSO;
+		SELECT count(*) INTO V_TOT_INSCRITO_CURSO_NOM_ANIO FROM tbl_matricula mat, tbl_curso c WHERE mat.cur_ID = c.cur_ID AND mat.Mat_DNI = p_Mat_DNI AND YEAR(c.cur_FechaInicio) = V_ANIO_CURSO;
+		
+		
+        IF (V_CONTADOR = 0 AND V_CONTADOR_CORREO = 0 AND V_COUNT_PROF_email = 0 AND V_TOT_INSCRITO_CURSO_NOM_ANIO < p_maximoReg) THEN
+                INSERT INTO `dbmatricula`.`tbl_matricula` (
+                cur_ID,Mat_Nombre, Mat_Paterno, Mat_Materno, Mat_TipoDocIdent,
+                 Mat_DNI, Mat_Profesion, Mat_Genero, Mat_Grado, Mat_Correo, Mat_Celular, Mat_RUC,
+                 Mat_Entidad, ubi_codigo, Mat_NivelGobierno, Mat_Area, Mat_AreaOpcional,
+                 Mat_Cargo, Mat_Clasificacion, Mat_ModoContrato, Mat_ContracOtros, Mat_Oficio,
+                 Mat_NombreArchivo, Mat_RutaArchivo,Mat_Password,Mat_idnumber,Mat_username)
+                VALUES (p_cur_ID,p_Mat_Nombre,p_Mat_Paterno, p_Mat_Materno,p_Mat_TipoDocIdent,p_Mat_DNI,p_Mat_Profesion ,p_Mat_Genero ,p_Mat_GradoAcade ,
+                p_Mat_Corroe,p_Mat_Celular,p_Mat_Ruc ,p_Mat_Entidad , CONCAT(p_Mat_Departamento ,p_Mat_Provincia ,p_Mat_Distrito)  ,
+                p_Mat_NivGobierno ,p_Mat_Area  ,p_Mat_AreaOpcional ,p_Mat_Cargo  ,p_Mat_Clasificacion  ,p_Mat_ModContrac  ,p_Mat_ContracOtros  ,
+                p_Mat_NroOficio  ,p_Mat_NomArchivo  ,p_Mat_RutaArchivo,p_Mat_DNI,p_Mat_DNI,p_Mat_DNI);
+                SELECT 'Sus datos han sido registrados con éxito. Sus datos pasarán a la etapa de evaluación y, de encontrarse apto, se le enviará a su correo electrónico la confirmación de su matrícula, así como el respectivo usuario y contraseña para que pueda acceder al curso.' INTO msg_salida;
+        END IF;
+
+        IF (V_CONTADOR != 0 )THEN
+                SELECT 'Ud. ya fue matriculado en este curso' INTO msg_salida;
+        END IF;
+        IF (V_CONTADOR_CORREO != 0 OR V_COUNT_PROF_email != 0)THEN
+                SELECT 'Ya existe un usuario registrado con este correo' INTO msg_salida;
+        END IF;
+		IF (V_TOT_INSCRITO_CURSO_NOM_ANIO >= p_maximoReg )THEN
+			-- SELECT CONCAT('Ud. ya fue matriculado ',V_TOT_INSCRITO_CURSO_NOM_ANIO,' veces', ' en el curso ',V_NOMBRE_CURSO,' durante el año', V_ANIO_CURSO) INTO msg_salida;
+            SELECT CONCAT('Usted ya participó en ', p_maximoReg, ' o más cursos en el presente año.', ' Para cualquier consulta al respecto de los cursos de capacitación puede escribirnos al correo de capacitacion@sbn.gob.pe') INTO msg_salida;
+            
+        END IF;
+    END ;;
+DELIMITER ;
---
+++ Pase_Produccion/2do_Entregable/Archivos_Finales_2do_Entregable/var/www/html/servicios/Matricula/Insert_matricula.php
@@ -0,0 +1,142 @@
+<?php
+//var_dump($_POST);
+//var_dump($_FILES);
+
+if (empty($_POST['txt_NOMBRE'])) {
+	$errors[] = "Ingresa el nombre.";
+}
+if (empty($_POST['txt_RUC_ENT'])) {
+	$errors[] = "Ingresa el RUC.";
+}
+if (empty($_POST['txt_DNI'])) {
+	$errors[] = "Ingresa el DNI.";
+} elseif (!empty($_POST['txt_NOMBRE'])) {
+	require_once("../../servicios/conexion.php"); //Contiene funcion que conecta a la base de datos
+
+	// escaping, additionally removing everything that could be (html/javascript-) code
+	$TXT_ID_CURSO = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["TXT_ID_CURSO"], ENT_QUOTES)));
+	$txt_NOMBRE = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["txt_NOMBRE"], ENT_QUOTES)));
+	$txt_APEPAT = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["txt_APEPAT"], ENT_QUOTES)));
+	$txt_APEMAT = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["txt_APEMAT"], ENT_QUOTES)));
+	$cboTipoDocIdentidad = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["cboTipoDocIdentidad"], ENT_QUOTES)));
+	$txt_DNI = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["txt_DNI"], ENT_QUOTES)));
+	$txt_PROFESION = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["txt_PROFESION"], ENT_QUOTES)));
+	$CBO_GENERO = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["CBO_GENERO"], ENT_QUOTES)));
+	$CBO_GRADO_ACAD = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["CBO_GRADO_ACAD"], ENT_QUOTES)));
+	$txt_CORREO = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["txt_CORREO"], ENT_QUOTES)));
+	$txt_TELEF = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["txt_TELEF"], ENT_QUOTES)));
+	$txt_RUC_ENT = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["txt_RUC_ENT"], ENT_QUOTES)));
+	$txt_ENTIDAD = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["txt_ENTIDAD"], ENT_QUOTES)));
+	$COD_DPTO_LAB = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["COD_DPTO_LAB"], ENT_QUOTES)));
+	$COD_PROV_LAB = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["COD_PROV_LAB"], ENT_QUOTES)));
+	$COD_DIST_LAB = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["COD_DIST_LAB"], ENT_QUOTES)));
+	$NIVEL_GOBIERNO = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["NIVEL_GOBIERNO"], ENT_QUOTES)));
+	$txt_area = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["txt_area"], ENT_QUOTES)));
+	$txt_areaopcional = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["txt_areaopcional"], ENT_QUOTES)));
+	$txt_CARGO = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["txt_CARGO"], ENT_QUOTES)));
+	$cbro_rubro = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["cbro_rubro"], ENT_QUOTES))); //clasificacion
+	$MODALIDAD = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["MODALIDAD"], ENT_QUOTES)));
+	$DESC_MODAL = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["DESC_MODAL"], ENT_QUOTES)));
+	$V_RUTA = mysqli_real_escape_string($mysqlConeccion, (strip_tags("/Archivos/PDF/", ENT_QUOTES)));
+	$txt_nro_of_asignado = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["txt_nro_of_asignado"], ENT_QUOTES)));
+	$TXT_VAL_OFICIO = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["TXT_VAL_OFICIO"], ENT_QUOTES)));
+	// $txf_adjuntar_oficio = mysqli_real_escape_string($mysqlConeccion,(strip_tags($_FILES["txf_adjuntar_oficio"],ENT_QUOTES)));
+	$DESC_MODAL = mysqli_real_escape_string($mysqlConeccion, (strip_tags($_POST["DESC_MODAL"], ENT_QUOTES)));
+	//	$stock = intval($_POST["stock"]);
+	//	$price = floatval($_POST["price"]);
+	$txf_adjuntar_oficio = (isset($_FILES['txf_adjuntar_oficio'])) ? $_FILES['txf_adjuntar_oficio'] : null;
+
+	$codarchivo = "";
+	$archrepe = 1;
+	if ($txf_adjuntar_oficio) {
+		$codarchivo = date('dmyhisv') . $txf_adjuntar_oficio['name'];
+		$files = glob('../..' . $V_RUTA . '*'); //obtenemos todos los nombres de los ficheros
+		foreach ($files as $file) {
+
+			$insarch = "../.." . $V_RUTA . $codarchivo;
+			//$data .=$file." ---- " .$insarch."---";
+			if ($file == $insarch) {
+				$archrepe = 0;
+				//	$data .=$archrepe;
+				break;
+			}
+			// unlink($file); //elimino el fichero
+		}
+		if ($archrepe != 0) {
+			// $ruta_destino_archivo = $_SERVER['DOCUMENT_ROOT'] . "/Archivos/PDF/{$txf_adjuntar_oficio['name']}";
+			// $archivo_ok = move_uploaded_file($txf_adjuntar_oficio['tmp_name'], $ruta_destino_archivo);
+
+			require_once $_SERVER['DOCUMENT_ROOT'] . '/app/config.php';
+			$connection = ssh2_connect(SFTP_HOST, SFTP_PORT);
+			if (ssh2_auth_password($connection, SFTP_USER, SFTP_PASS)) {
+				$rutaUpdoad = $_SERVER['DOCUMENT_ROOT'] . '/Archivos/PDF/';
+				ssh2_scp_send($connection, $txf_adjuntar_oficio['tmp_name'], $rutaUpdoad . $codarchivo, 0644);
+			}
+		}
+	}
+	$data = "";
+
+	// REGISTER data into database
+	if ($archrepe != 0) {
+		$xLimiteMatricula = 2;
+		//$sql = "call sp_ins_matricula('$TXT_ID_CURSO', '$txt_NOMBRE', '$txt_APEPAT', '$txt_APEMAT','$cboTipoDocIdentidad', '$txt_DNI', '$txt_PROFESION', '$CBO_GENERO', '$CBO_GRADO_ACAD', '$txt_CORREO', '$txt_TELEF', '$txt_RUC_ENT', '$txt_ENTIDAD', '$COD_DPTO_LAB','$COD_PROV_LAB','$COD_DIST_LAB', '$NIVEL_GOBIERNO', '$txt_area','$txt_areaopcional', '$txt_CARGO', '$cbro_rubro', '$MODALIDAD', '$DESC_MODAL', '$txt_nro_of_asignado', '$codarchivo', '$V_RUTA',@message);";
+		//$sql = "call sp_ins_matricula_v2('$TXT_ID_CURSO', '$txt_NOMBRE', '$txt_APEPAT', '$txt_APEMAT','$cboTipoDocIdentidad', '$txt_DNI', '$txt_PROFESION', '$CBO_GENERO', '$CBO_GRADO_ACAD', '$txt_CORREO', '$txt_TELEF', '$txt_RUC_ENT', '$txt_ENTIDAD', '$COD_DPTO_LAB','$COD_PROV_LAB','$COD_DIST_LAB', '$NIVEL_GOBIERNO', '$txt_area','$txt_areaopcional', '$txt_CARGO', '$cbro_rubro', '$MODALIDAD', '$DESC_MODAL', '$txt_nro_of_asignado', '$codarchivo', '$V_RUTA',@message);";		
+		$sql = "call sp_ins_matricula_v3('$TXT_ID_CURSO', '$txt_NOMBRE', '$txt_APEPAT', '$txt_APEMAT','$cboTipoDocIdentidad', '$txt_DNI', '$txt_PROFESION', '$CBO_GENERO', '$CBO_GRADO_ACAD', '$txt_CORREO', '$txt_TELEF', '$txt_RUC_ENT', '$txt_ENTIDAD', '$COD_DPTO_LAB','$COD_PROV_LAB','$COD_DIST_LAB', '$NIVEL_GOBIERNO', '$txt_area','$txt_areaopcional', '$txt_CARGO', '$cbro_rubro', '$MODALIDAD', '$DESC_MODAL', '$txt_nro_of_asignado', '$codarchivo', '$V_RUTA', '$xLimiteMatricula', @message);";		
+		$query = mysqli_query($mysqlConeccion, $sql);
+
+		// if product has been added successfully
+
+	}
+
+
+	if ($archrepe != 0) {
+
+		$rpta = mysqli_query($mysqlConeccion, "select @message;");
+
+		// 	$msgArray = mysqli_fetch_assoc($rpta)
+		while ($row = mysqli_fetch_array($rpta)) {
+			$matriculas[] = $row;
+		}
+		foreach ($matriculas as $datosMatricula) {
+			$data = $datosMatricula[0];
+		}
+	}
+	// echo $rpta;
+	// die();
+	if ($archrepe == 0) {
+		$messages[] = "Nombre de archivo repetido";
+	}
+	if ($archrepe != 0) {
+		if ($query) {
+			$messages[] = $data;
+		} else {
+			$errors[] = "Lo sentimos, el registro falló. Por favor, regrese y vuelva a intentarlo.";
+		}
+	}
+} else {
+	$errors[] = "desconocido.";
+}
+if (isset($errors)) {
+
+?>
+
+					<?php
+					foreach ($errors as $error) {
+						echo $error;
+					}
+					?>
+			<?php
+		}
+		if (isset($messages)) {
+
+			?>
+	
+						<?php
+						foreach ($messages as $message) {
+							echo $message;
+						}
+						?>
+
+				<?php
+			}
+				?>
